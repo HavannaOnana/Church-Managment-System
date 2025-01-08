04e039c0806d06b833028d9ed83df23fbb89a33b
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-08 12:44:47 +0100

    Database file added

---
+++ includes/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database{
+    public $pdo;
+    public $stmt;
+
+    public function __construct($db='church',$host = "localhost:3310",$user='root',$pwd=''){
+        try{
+            $this->pdo = new PDO("mysqlhost:host=$host;dbname=$db",$user,$pwd);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e){
+            echo "Connection failed : ".$e->getMessage();
+        }
+    }
+
+    public function run($query, $param = null){
+        $this->stmt = $this->pdo->prepare($query);
+        if($param!= null){
+            $this->stmt->execute($param);
+        }
+        return $this->stmt->execute();
+    }
+    
+}
+
+
+?>
