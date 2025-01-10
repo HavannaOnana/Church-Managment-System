10b2cfe403f0b7c4f242f6ac1031b66d9e718a40
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-10 10:09:31 +0100

    added the constructor

--- Church/Church.php
+++ Church/Church.php
@@ -1,8 +1,20 @@
 <?php
 
-include_once "./includes/Database.php"
+include_once "./includes/Database.php";
 
 
+class Church{
+
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    
+
+
+}
 
 
 
