0a07deb9dccf1c46b65a3432ccace67caa2593ad
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-10 10:25:09 +0100

    added a user in the church Class

--- Church/Church.php
+++ Church/Church.php
@@ -11,6 +11,17 @@ class Church{
         $this->db = new Database();
     }
 
+    public function AddUser($name,$age,$email,$phonenumber){
+        $sql = "INSERT INTO users(name,age,email,phonenumber) VALUES(:name,:age,:email,:phonenumber)";
+        $param = [
+            ":name"=>$name,
+            ":age"=>$age,
+            ":email"=>$email,
+            "phonenumber"=>$phonenumber
+        ];
+        return $this->db->run($sql,$param);
+    }
+    
     
 
 
