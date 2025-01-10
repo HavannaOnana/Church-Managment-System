cdcc0985a1c4610cfd0030ed78e821f4bc5d25bb
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-10 14:18:52 +0100

    sign up and css added

---
+++ signup.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In Page</title>
+    <link rel="stylesheet" href="./css/signup.css">
+</head>
+<body>
+    <div class="form">
+        <img src="./images/signout.png" alt="">
+        <form action="" method="post">
+            <img src="./images/arise-logo-150.png" alt="">
+            <h1>welcome back.</h1>
+            <label for="email" name="email"></label><br>
+            <input type="text" placeholder="email"><br>
+            <h2>&</h2>
+            <label for="password" name="password"></label><br>
+            <input type="text" placeholder="password"><br><br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
