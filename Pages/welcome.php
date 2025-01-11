2d856b4f475e68afd41ea17f5a4d963180eec356
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-11 19:15:27 +0100

    added navbar to welcome page

---
+++ Pages/welcome.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Admin</title>
+    <link rel="stylesheet" href="../css/welcome.css">
+</head>
+<body>
+
+     <div class="navbar">
+        <img src="../images/arise-logo-150.png" alt="">
+        <a href="#">attendance</a>
+        <a href="#">rankings</a>
+        <a href="#">graphs</a>
+        <a href="#">members</a>
+        <button type="button" id="export">Export</button>
+     </div>
+    
+</body>
+</html>
