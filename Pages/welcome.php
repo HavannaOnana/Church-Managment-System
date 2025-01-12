3714ca8ba5daeb0c2499628c62f7abd353e45bab
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-12 12:34:53 +0100

    added the date so you can chnage the date

--- Pages/welcome.php
+++ Pages/welcome.php
@@ -1,5 +1,16 @@
 <?php
 
+// time code and everything 
+$currentDate = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+
+
+$timestamp = strtotime($currentDate);
+
+$displayDate = date('l, F j, Y', $timestamp);
+
+$previousSunday = date('Y-m-d', strtotime('-7 days', $timestamp));
+$nextSunday = date('Y-m-d', strtotime('+7 days', $timestamp));
+
 
 ?>
 
@@ -21,6 +32,23 @@
         <a href="../Pages/members.php">members</a>
         <button type="button" id="export">Export</button>
      </div>
+
+    <div class="attendance">
+    <div class="date">
+        <a href="?date=<?php echo $previousSunday; ?>">
+            <ion-icon name="caret-back-outline" class="backward"></ion-icon>
+        </a>
+
+        <h1 class="date"><?php echo $displayDate; ?></h1>
+
+        <a href="?date=<?php echo $nextSunday; ?>">
+            <ion-icon name="caret-forward-outline" class="forward"></ion-icon>
+        </a>
+    </div>
+</div>
     
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
